ccba334a78456a5383089a62c96f21378b2ddb22
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-10-06 21:26:37 +0300

    add updateById for current user

--- controllers/contacts/updateContactById.js
+++ controllers/contacts/updateContactById.js
@@ -3,10 +3,18 @@ const { Contact } = require("../../models");
 const { sendSuccessResponse } = require("../../utils");
 
 const updateContactById = async (req, res) => {
+  const { _id } = req.user;
   const { contactId } = req.params;
-  const contact = await Contact.findByIdAndUpdate(contactId, req.body, {
-    new: true,
-  });
+  const contact = await Contact.findOneAndUpdate(
+    {
+      _id: contactId,
+      owner: _id,
+    },
+    req.body,
+    {
+      new: true,
+    }
+  );
   if (!contact) {
     throw new NotFound(`Contact with id=${contactId} not found`);
   }
