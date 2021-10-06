37fa29a7882cdb68d356b2028065e3e8c02c3a9e
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-10-06 21:18:50 +0300

    add controller getContactById for current user

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -3,11 +3,13 @@ const { Contact } = require("../../models");
 const { sendSuccessResponse } = require("../../utils");
 
 const getContactById = async (req, res) => {
+  const { _id } = req.user;
   const { contactId } = req.params;
-  const contact = await Contact.findById(
-    contactId,
-    "_id name email phone favorite"
+  const contact = await Contact.findOne(
+    { _id: contactId, owner: _id },
+    "_id name email phone favorite owner"
   );
+  console.log(contact);
   if (!contact) {
     throw new NotFound(`Contact with id=${contactId} not found`);
   }
