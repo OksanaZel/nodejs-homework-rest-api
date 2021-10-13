d5fd3e39f3d12725ff89ed4b5675dec09db8eace
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-10-13 17:24:49 +0300

    Add patch router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,7 @@ const {
   controllerWrapper,
   validation,
   authenticate,
+  upload,
 } = require("../../middlewares");
 const { userSchemaJoi } = require("../../models/user");
 
@@ -16,5 +17,11 @@ router.post(
 router.post("/login", validation(userSchemaJoi), controllerWrapper(ctrl.login));
 router.get("/logout", authenticate, controllerWrapper(ctrl.logout));
 router.get("/current", authenticate, controllerWrapper(ctrl.current));
+router.patch(
+  "/avatar",
+  authenticate,
+  upload.single("avatar"),
+  controllerWrapper(ctrl.avatars)
+);
 
 module.exports = router;
